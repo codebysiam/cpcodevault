813fc8981f05e37d302000e310ef038d2f3d1794
CodeBySiam <codebysiam@gmail.com>
2026-09-08 02:08:58 +0600

    cf: Ultra-Fast Mathematician (61A)

---
+++ Codeforces/ultra_fast_mathematician_61A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    string s1, s2;
+    cin >> s1 >> s2;
+    for(int i = 0; i<s1.size(); i++){
+        cout << (s1[i] != s2[i] ? 1 : 0);
+    }
+    return 0;
+}
