eadb305427f47986132b9fc60785e3168edc81f8
CodeBySiam <codebysiam@gmail.com>
2026-09-09 02:15:43 +0600

    cf: Anton and Polyhedrons (785A)

---
+++ Codeforces/anton_and_polyhedrons_785A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int n;
+    cin >> n;
+    int ans = 0;
+    while(n--){
+        string s;
+        cin >> s;
+        if(s == "Tetrahedron") ans+=4;
+        if(s == "Cube") ans+=6;
+        if(s == "Octahedron") ans+=8;
+        if(s == "Dodecahedron") ans+=12;
+        if(s == "Icosahedron") ans+=20;
+    }
+    cout << ans;
+    return 0;
+}
